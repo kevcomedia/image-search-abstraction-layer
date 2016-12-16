e7075625d384208d7fd1230efc43d819dff87be7
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-16 15:27:47 +0000

    Store search term to database

--- app/api.js
+++ app/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('request');
 
-module.exports = function() {
+module.exports = function(models) {
   const api = express.Router();
 
   api.get('/imagesearch/:searchString', function(req, res) {
@@ -11,17 +11,24 @@ module.exports = function() {
       + `&cx=${process.env.SEARCH_ENGINE_ID}`
       + `&key=${process.env.API_KEY}`
       + (req.query.offset ? `&start=${req.query.offset}` : '');
-    request(searchUrl, function(err, response, body) {
-      // TODO write a better way to handle `err`
-      if (err) return console.error('hi', err);
 
-      var json = JSON.parse(body).items
-        .map(o => ({
-          imageUrl: o.link,
-          altText: o.snippet,
-          pageUrl: o.image.contextLink
-        }));
-      res.json(json);
+    Promise.resolve(models.Recent.create({
+      query: req.params.searchString,
+      timestamp: new Date()
+    }))
+    .then(function fulfilled() {
+      request(searchUrl, function(err, response, body) {
+        // TODO write a better way to handle `err`
+        if (err) return console.error('hi', err);
+
+        var json = JSON.parse(body).items
+          .map(o => ({
+            imageUrl: o.link,
+            altText: o.snippet,
+            pageUrl: o.image.contextLink
+          }));
+        res.json(json);
+      });
     });
   });
 
