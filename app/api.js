5a93e44117062c23125bdde0a4d61d08f725cea4
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-16 14:41:15 +0000

    Write request for custom search

--- app/api.js
+++ app/api.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const request = require('request');
 
 module.exports = function() {
   const api = express.Router();
 
   api.get('/imagesearch/:searchString', function(req, res) {
-    res.send(req.params.searchString);
+    const searchUrl = 'https://www.googleapis.com/customsearch/v1?'
+      + `&q=${req.params.searchString}`
+      + '&searchType=image'
+      + `&cx=${process.env.SEARCH_ENGINE_ID}`
+      + `&key=${process.env.API_KEY}`;
+    request(searchUrl, function(err, response, body) {
+      // TODO write a better way to handle `err`
+      if (err) return console.error('hi', err);
+      res.json(JSON.parse(body).items);
+    });
   });
 
   api.get('/recent', function(req, res) {
